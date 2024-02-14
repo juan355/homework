2201f6902f0d86116b042413c57c6e213dfd927b
juan355 <71080279+juan355@users.noreply.github.com>
2024-02-14 04:34:14 +0000

    Project 7.3

---
+++ TowAndTen2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num = 0;
+    for(int i =0;i<11;i++){
+        cout<<"Number: "<<num<<"\n";
+         cout<<"Multiplied by 2: "<<num*2<<"\n";
+          cout<<"Multiplied by 10: "<<num*10<<"\n";
+          num++;
+    }
+    return 0;
+}
