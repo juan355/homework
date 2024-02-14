c8ec657cc526d9728ba02ce80163797026a780df
juan355 <71080279+juan355@users.noreply.github.com>
2024-02-14 04:37:32 +0000

    project 7.4

---
+++ TwoAndTen3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num = 0;
+    while(num<10){
+        cout<<"Number: "<<num<<"\n";
+         cout<<"Multiplied by 2: "<<num*2<<"\n";
+          cout<<"Multiplied by 10: "<<num*10<<"\n";
+          num++;
+    }
+    return 0;
+}
