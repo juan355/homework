cba6051880a1530214b5f788994fec6bbc2dd0f8
juan355 <71080279+juan355@users.noreply.github.com>
2024-02-20 05:40:44 +0000

    Project 8.2

---
+++ Class.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string name[20];
+    int age[20];
+    char grade[20];
+    int num;
+    cout << "Please enter number of students";
+    cin >> num;
+    for(int i =0;i<num;i++){
+        cout <<"Enter name: ";
+        cin>> name[i];
+        cout << "Enter age: ";
+        cin >> age[i];
+        if(age[i]<0){
+            cout <<"Invaled age enter agin"<<"\n";
+            cout << "Enter age: ";
+            cin >> age[i];  
+        }
+        cout <<"Enter grade ";
+        cin >> grade[i];
+        if(grade[i]!= 65){
+            if(grade[i]!= 66){
+            if(grade[i]!= 67){
+            if(grade[i]!= 68){
+            if(grade[i]!= 70){
+            cout <<"Invaled grade enter agin"<<"\n";
+            cout << "Enter grade: ";
+            cin >> grade[i]; 
+            }
+            }
+            }
+            }
+        } 
+        
+    }
+
+}
